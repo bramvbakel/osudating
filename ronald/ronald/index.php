54e884dfcb17243fe148a0af5f4375f3d1f4265e
Bram van Bakel <110277533+bramvbakel@users.noreply.github.com>
2022-09-08 11:26:26 +0200

    Add files via upload

---
+++ ronald/ronald/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Osudating</title>
+</head>
+
+<body>
+
+    <h1>Welcome</h1>
+    <p>Wow this site is so cool, I hope it will stay so cool!</p>
+    <p>This site is my way of learning webdev and stuff.</p>
+
+    <header>
+        <h2>PC Rhythm Games</h2>	<!--Hier komen alle PC rhythm games voor de site, geen simulators die komen later!-->
+        <p><a href="https://osudating.eu/osu" target="_blank">osu!</a></p>
+        <p><a href="https://osudating.eu/etterna" target="_blank">Etterna</a></p>
+        <p><a href="https://osudating.eu/stepmania" target="_blank">Stepmania</a></p>
+        <p><a href="https://osudating.eu/adofai" target="_blank">A Dance of Fire and Ice</a></p>
+        <p><a href="https://osudating.eu/fnf" target="_blank">Friday Night Funkin'</a></p>
+    </header>
+
+    <header>
+        <h2>Arcade Rhythm Games</h2>
+        <p><a href="https://remywiki.com/What_is_SOUND_VOLTEX">Sound Voltex</a></p> <!-- Ff kijken -->
+        <p><a href="https://remywiki.com/What_is_beatmania_IIDX">Beatmania IIDX</a></p>
+        <!-- IIDX.org linken, mogelijk subpage maken -->
+        <p><a href="https://en.wikipedia.org/wiki/Taiko_no_Tatsujin">Taiko no Tatsujin!</a></p>
+        <!-- tjadataba.se linken, mogelijk subpage -->
+        <p><a href="https://en.wikipedia.org/wiki/Groove_Coaster">Groove Coaster</a></p> <!-- Subpage maken -->
+        <p><a href="https://wacca.marv.jp">Wacca</a></p> <!-- Subpage maken -->
+        <p><a href="https://en.wikipedia.org/wiki/Pop%27n_Music">Pop'n Music</a></p> <!-- Subpage maken i guess -->
+        <p><a href="https://remywiki.com/AC_jb">Jubeat</a></p> <!-- ?????? -->
+        <p><a href="https://maimai.sega.com">MaiMai</a></p> <!-- Subpage maken, songlist erbij -->
+        <p><a href="https://chunithm.sega.com">Chunithm</a></p> <!-- Subpage maken -->
+        <p><a href="https://en.wikipedia.org/wiki/In_the_Groove_2">In the Groove 2</a></p>
+        <!-- Subpage maken die ook werkt met NITG -->
+        <p><a href="https://remywiki.com/AC_DRS">DanceRush Stardom</a></p> <!-- Subpage maken -->
+        <p><a href="https://remywiki.com/What_is_NOSTALGIA">Nostalgia</a></p> <!-- Piano tiles linken -->
+        <p><a href="https://miku.sega.jp/arcade/en/">Project Diva Arcade</a></p> <!-- Subpage maken -->
+    </header>
+
+    <header>
+        <h2>Mobile Rhythm Games</h2>
+        <p><a href="https://arcaea.lowiro.com/en">Arcaea</a></p> <!-- subpage maken -->
+        <p><a
+                href="https://media.discordapp.net/attachments/530365678042218530/972483690250780702/unknown.png">Lanota</a>
+        </p> <!-- Nieuwe meme in gooien -->
+        <p><a href="https://pjsekai.sega.jp">Project Sekai</a></p> <!-- Dit ff bekijken -->
+        <p><a href="https://groovecoaster.com/apps/en/">Groove Coaster</a></p>
+        <!-- Dit ff bekijken, mis gewoon 1 page maken -->
+    </header>
+
+    <header>
+        <h2>Console Rhythm Games</h2>
+        <p><a href="https://www.nintendo.com/store/products/groove-coaster-wai-wai-party-switch/">Groove Coaster WaiWai
+                Party</a></p> <!-- Dit ff bekijken, mis gewoon 1 page maken -->
+        <p><a href="https://www.nintendo.com/store/products/taiko-no-tatsujin-drum-n-fun-switch/">Taiko no Tatsujin!</a>
+        </p> <!-- taiko databa.se linken mogelijk subpage -->
+        <p><a href="https://www.nintendo.com/store/products/hatsune-miku-project-diva-mega-mix-switch/">Project Diva</a>
+        </p> <!-- Subpage maken -->
+    </header>
+
+    <header>
+        <h2>Simulators</h2>
+        <p><a href="https://www.kshootmania.com/en/">K-Shootmania</a></p>
+        <!-- Subpage ,alem. mis samen met sdvx gooien -->
+        <p><a href="https://github.com/Drewol/unnamed-sdvx-clone">USC</a></p> <!-- Zelfde als hierboven -->
+        <p><a href="https://clonehero.net">Clone Hero</a></p> <!-- Clone hero linken, mogelijk subpage -->
+        <p><a href="https://bms-community.github.io/resources/">BMS</a></p> <!-- Page al gemaakt, niet af -->
+    </header>
+
+    <header>
+        <h2>Controllers</h2>
+        <p><a href="https://yuancon.store">Yuancon</a></p> <!-- Yuancon linken, mogelijk subpage -->
+        <p><a href="https://www.gamo2.com/en/">Gamo2</a></p> <!-- Gamo2 linken, mogelijk subpage -->
+    </header>
+
+</body>
+
+</html>
